38d7b41d571faa8f4c8dd4ff2c219dc34ee81def
asheesh goja <axgoja@ups.com>
2020-12-01 17:42:32 -0800

    changed kubernetes api version

--- build-n-deploy.sh
+++ build-n-deploy.sh
@@ -1,10 +1,11 @@
 npm install -g @angular/cli
+npm install
 ng build --prod --source-map
 rm -fr socrates_bff/www/
-cp -r www/ socrates_bff/
+cp -r www/ socrates_bff/www
 cd socrates_bff
-docker build -t gcr.io/$DEVSHELL_PROJECT_ID/clips-socrates:latest .
-gcloud docker -- push gcr.io/$DEVSHELL_PROJECT_ID/clips-socrates:latest
+docker build -t gcr.io/ups-next-tracking-as-a-service/clips-socrates:latest .
+gcloud docker -- push gcr.io/ups-next-tracking-as-a-service/clips-socrates:latest
 
 kubectl scale --replicas=0 deployment/socrates-socket-server-deployment
 kubectl scale --replicas=1 deployment/socrates-socket-server-deployment
